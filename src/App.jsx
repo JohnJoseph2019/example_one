779a2816edd662729d8740fc3972a72de75fe00b
Juan Delgado <Delgado-juan@hotmail.com>
2024-02-20 21:46:11 -0500

    changes 14

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import './App.css';
 
 function App() {
     return (
-        <Router>
+        <Router basename={'/example_one'}>
             <MyNav />
             <Routes>
                 <Route path='/' element={<Home />} />
