e534dea51f72a042abb6c5b3e2dda8438df9e2c6
Juan Delgado <Delgado-juan@hotmail.com>
2024-02-20 21:06:36 -0500

    changes4

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,18 +4,18 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function myNav() {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">My React Portfolio</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="./">Home</Nav.Link>
-            <Nav.Link href="./about">About</Nav.Link>
-            <Nav.Link href="./portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="./contact">Contact</Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+    return (
+        <Navbar expand='lg' className='bg-body-tertiary'>
+            <Container>
+                <Navbar.Brand href='#home'>My React Portfolio</Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='me-auto'>
+                        <Nav.Link href='./'>Home</Nav.Link>
+                        <Nav.Link href='/about'>About</Nav.Link>
+                        <Nav.Link href='/portfolio'>Portfolio</Nav.Link>
+                        <Nav.Link href='/contact'>Contact</Nav.Link>
+                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -26,11 +26,11 @@ function myNav() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown> */}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default myNav;
