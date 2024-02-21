d2b3e6aed649590e95ff3868b0fce82c102e5155
Juan Delgado <Delgado-juan@hotmail.com>
2024-02-20 21:40:33 -0500

    changes 12

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import './App.css';
 
 function App() {
     return (
-        <Router basename={import.meta.env.DEV ? '/' : '/example_one/'}>
+        <Router>
             <MyNav />
             <Routes>
                 <Route path='/' element={<Home />} />
