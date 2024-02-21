83cd1e4bf8195ad5dc6032f13215c87bed3fe9be
Juan Delgado <Delgado-juan@hotmail.com>
2024-02-20 20:33:40 -0500

    changes routers deploment code for githubpages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MyNav from './components/Navbar'
-import { Contact, Custom404, Home, ProjectGallery, About } from './components/Pages'
-import './App.css'
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import MyNav from './components/Navbar';
+import {
+    Contact,
+    Custom404,
+    Home,
+    ProjectGallery,
+    About,
+} from './components/Pages';
+import './App.css';
 
 function App() {
-
-  return (
-    <Router>
-      <MyNav />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/portfolio' element={<ProjectGallery />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/404' element={<Custom404 />} />
-        </Routes>
-    </Router>
-  )
+    return (
+        <Router basename={import.meta.env.DEV ? '/' : '/example_one/'}>
+            <MyNav />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/portfolio' element={<ProjectGallery />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/404' element={<Custom404 />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -10,15 +10,32 @@
 //   plugins: [react()],
 // })
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true
+//   }
+// })
+
+// vite.config.js
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
-})
+export default defineConfig(({command}) => {
+    const config = {
+        plugins: [react()],
+        base: '/',
+    };
+
+    if (command !== 'serve') {
+        config.base = '/example_one/';
+    }
+
+    return config;
+});
